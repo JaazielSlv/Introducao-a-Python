4ee42fc4bebc575ac49956bb1ad94affcdbe7176
JaazielSlv <143227938+JaazielSlv@users.noreply.github.com>
2023-08-28 09:07:04 -0300

    Update atividade3.py

--- AtividadeFacu/atividade3.py
+++ AtividadeFacu/atividade3.py
@@ -1,7 +1,5 @@
-# prod = v1(Valor 1) * v2(Valor 2)
-
 v1 = float(input("Primerio valor: "))
 v2 = float(input("Segundo valor: "))
 
 prod = v1 * v2
-print("Resultado", prod)
+print("Resultado {}".format(prod))
