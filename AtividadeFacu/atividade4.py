6c309a3af361c0d05e381dabab83ddeec39f87a2
JaazielSlv <143227938+JaazielSlv@users.noreply.github.com>
2023-08-28 09:11:26 -0300

    Update atividade4.py

--- AtividadeFacu/atividade4.py
+++ AtividadeFacu/atividade4.py
@@ -1,8 +1,6 @@
-# medida = v1(Valor 1)**3.5 + v2(Valor 2)**7.5
-
 v1 = float(input("Primerio valor: ")) * 3.5
 v2 = float(input("Segundo valor: ")) * 7.5
 
 media = (v1 + v2) / 11
 
-print("Resultado", media)
+print("Resultado {:.5f}".format(media))
