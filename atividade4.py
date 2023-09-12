0bd7d0af11018bc2ee9d28a957907d432fe0baf3
JaazielSlv <143227938+JaazielSlv@users.noreply.github.com>
2023-09-12 19:33:13 -0300

    Delete atividade4.py

--- atividade4.py
+++
@@ -1,8 +0,0 @@
-hi=int(input('Digite o valor da hora inicial '))
-hf=int(input('Digite o valor da hora final '))
-
-if hi==hf:print('O jogo durou 24 Horas')
-else:
-    if hi<hf:print('O jogo durou',hf-hi,'Hora (S)')
-    else:    
-        if hi>hf:print('O jogo durou',(24-hi)+hf, 'Hora (S)')
